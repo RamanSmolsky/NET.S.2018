84aaa9a4e80875f4bd7821250dbae5d06fe9764b
Raman.Smolsky <Raman.Smolsky@gmail.com>
2018-03-25 17:48:22 +0300

    added initial solution for the task #1 (Polynoms)

---
+++ NET.S.2018.Smolsky.06/Polynom/Polynom.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolynomMath
+{
+    /// <summary>
+    /// Handles polynomial expression of one variable; Polynom order is defined by the number of coefficients
+    /// </summary>
+    public class Polynom
+    {
+        #region public members
+
+        public Polynom() : this(0.0)
+        {
+        }
+
+        public Polynom(int coefficient)
+        {
+            Coefficients = new double[] { coefficient };
+
+            Order = Coefficients.Length - 1;
+        }
+
+        public Polynom(params double[] coefficients)
+        {
+            Coefficients = coefficients;
+
+            Order = Coefficients.Length - 1;
+        }
+
+        public int Order { get; private set; }
+
+        public double[] Coefficients { get; private set; }
+
+        public static Polynom operator +(Polynom p1, Polynom p2)
+        {
+            return new Polynom(0);
+        }
+
+        #endregion public members
+    }
+}
---
+++ NET.S.2018.Smolsky.06/PolynomMathTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PolynomMathTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PolynomMathTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fcbfa8e4-cc86-4659-88db-45f3d1a0dd8a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
