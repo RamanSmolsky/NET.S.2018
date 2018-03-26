22d9bab705c4c8cc68e2a2c228b9d6d51e78c578
Raman.Smolsky <Raman.Smolsky@gmail.com>
2018-03-26 22:20:47 +0300

    task #1 (Polynoms) updates - added minimal support for +, ==, != operators; Equals, GetHashCode methods

--- NET.S.2018.Smolsky.06/Polynom/Polynom.cs
+++ NET.S.2018.Smolsky.06/Polynom/Polynom.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PolynomMath
 {
@@ -12,6 +9,8 @@ namespace PolynomMath
     public class Polynom
     {
         #region public members
+
+        public const double AllowedDifferenceOnComparison = 0.0000000000000009;
 
         public Polynom() : this(0.0)
         {
@@ -35,11 +34,129 @@ namespace PolynomMath
 
         public double[] Coefficients { get; private set; }
 
-        public static Polynom operator +(Polynom p1, Polynom p2)
+        public static Polynom operator +(Polynom lhs, Polynom rhs)
         {
-            return new Polynom(0);
+            if (lhs is null || rhs is null)
+            {
+                throw new ArgumentNullException($"{nameof(lhs)} or {nameof(rhs)} or both are null");
+            }
+            ////TODO: check for overflow
+            if (lhs.Order == rhs.Order)
+            {
+                double[] coefTemp = new double[lhs.Order];
+
+                int numOfCoef = lhs.Coefficients.Length;
+
+                for (int i = 0; i < numOfCoef - 1; i++)
+                {
+                    coefTemp[i] = lhs.Coefficients[i] + rhs.Coefficients[i];
+                }
+
+                return new Polynom(numOfCoef);
+            }
+
+            return AddSmallerPolynomToBigger(lhs, rhs);
+        }
+
+        public static bool operator ==(Polynom lhs, Polynom rhs)
+        {
+            if (lhs is null || rhs is null)
+            {
+                throw new ArgumentNullException($"{nameof(lhs)} or {nameof(rhs)} or both are null");
+            }
+
+            if (lhs.Order != rhs.Order)
+            {
+                return false;
+            }
+
+            return AreCoefficientsEqual(lhs.Coefficients, rhs.Coefficients);
+        }
+
+        public static bool operator !=(Polynom lhs, Polynom rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var polynom = obj as Polynom;
+            return !(polynom is null) &&
+                   Order == polynom.Order &&
+                   Coefficients.Length == polynom.Coefficients.Length &&
+                   AreCoefficientsEqual(Coefficients, polynom.Coefficients);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -113512862;
+            hashCode = (hashCode * -1521134295) + Order.GetHashCode();
+            hashCode = (hashCode * -1521134295) + EqualityComparer<double[]>.Default.GetHashCode(Coefficients);
+            return hashCode;
         }
 
         #endregion public members
+
+        #region private members
+
+        private static Polynom AddSmallerPolynomToBigger(Polynom smaller, Polynom bigger)
+        {
+            if (smaller is null || bigger is null)
+            {
+                throw new ArgumentNullException($"{nameof(smaller)} or {nameof(bigger)} or both are null");
+            }
+
+            if (smaller.Order < bigger.Order)
+            {
+                double[] coefTemp = new double[bigger.Coefficients.Length];
+                int numOfElemInShorterPolynom = smaller.Coefficients.Length;
+                int numOfElemInLongerPolynom = bigger.Coefficients.Length;                
+
+                for (int i = 0; i < numOfElemInShorterPolynom; i++)
+                {
+                    coefTemp[i] = smaller.Coefficients[i] + bigger.Coefficients[i];
+                }
+
+                int remainedPart = numOfElemInLongerPolynom - (numOfElemInLongerPolynom - numOfElemInShorterPolynom);
+
+                for (int i = remainedPart; i < numOfElemInLongerPolynom; i++)
+                {
+                    coefTemp[i] = bigger.Coefficients[i];
+                }
+
+                return new Polynom(coefTemp);
+            }
+            else
+            {
+                return AddSmallerPolynomToBigger(bigger, smaller);
+            }            
+        }
+
+        private static bool AreCoefficientsEqual(double[] coefLhs, double[] coefRhs)
+        {
+            if (coefLhs is null || coefRhs is null)
+            {
+                throw new ArgumentNullException($"{nameof(coefLhs)} or {nameof(coefRhs)} or both are null");
+            }
+
+            if (coefLhs.Length != coefRhs.Length)
+            {
+                return false;
+            }
+
+            int numOfCoef = coefLhs.Length;
+
+            for (int i = 0; i < numOfCoef - 1; i++)
+            {
+                if (Math.Abs(coefLhs[i] - coefRhs[i]) > AllowedDifferenceOnComparison)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        #endregion private members
     }
 }
---
+++ NET.S.2018.Smolsky.06/PolynomMathTests/PolynomMathAndMethodsTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using NUnit.Framework;
+
+using PolynomMath;
+
+namespace PolynomMathTests
+{
+    [TestFixture]
+    public class PolynomMathAndMethodsTests
+    {
+        private const string RequiredExceptionNotThrownMsg = "exception of required type was not thrown";
+
+        [Test]
+        public void PolynomOfZeroOrderTest()
+        {
+            Polynom p = new Polynom();
+            Assert.AreEqual(0, p.Order);
+        }
+
+        [Test]
+        public void PolynomOfFirstOrderTest()
+        {
+            Polynom p = new Polynom(1, 2);
+            Assert.AreEqual(1, p.Order);
+        }
+
+        [Test]
+        public void PolynomsOfFirstOrderSumTest()
+        {
+            Polynom p1 = new Polynom(1);
+            Polynom p2 = new Polynom(2);
+            Polynom result = p1 + p2;
+            Assert.AreEqual(0, result.Order);
+        }
+
+        [Test]
+        public void PolynomOfFirstOrderEqualsToItselfTest()
+        {
+            Polynom p = new Polynom(1.000007, 2.000003);
+            Assert.True(p.Equals(p), "polynom should be qual to itself");
+        }
+
+        [Test]
+        public void PolynomOfFirstOrderIsNotEqualToZeroOrderPolynomTest()
+        {
+            Polynom p1 = new Polynom(1);
+            Polynom p2 = new Polynom(1, 2);
+            Assert.False(p1.Equals(p2), "polynoms should be not marked as not equal");
+        }
+
+        [Test]
+        public void PolynomsLeftOperandInAdditionIsNullTest()
+        {
+            Polynom p1 = null;
+            Polynom p2 = new Polynom(2);
+            Polynom result;
+            Assert.Catch(typeof(ArgumentNullException), () => { result = p1 + p2; }, RequiredExceptionNotThrownMsg);
+        }
+
+        [Test]
+        public void PolynomsRightOperandInAdditionIsNullTest()
+        {
+            Polynom p1 = new Polynom(2);
+            Polynom p2 = null;
+            Polynom result;
+            Assert.Catch(typeof(ArgumentNullException), () => { result = p1 + p2; }, RequiredExceptionNotThrownMsg);
+        }
+
+        [Test]
+        public void SumOfZeroOrderPolynomsTest()
+        {
+            Polynom p1 = new Polynom(1);
+            Polynom p2 = new Polynom(2);
+            Polynom result = p1 + p2;
+        }
+
+        [Test]
+        public void SumOfSecondOrderPolynomsTest()
+        {
+            Polynom p1 = new Polynom(1, 2, 3);
+            Polynom p2 = new Polynom(1, 2, 3);
+            Polynom resExpected = new Polynom(2, 4, 6);
+
+            Polynom result = p1 + p2;                        
+        }
+
+        [Test]
+        public void SumOfFirstAndSecondOrderPolynomsTest()
+        {
+            Polynom p1 = new Polynom(1, 2);
+            Polynom p2 = new Polynom(1, 2, 3);
+            Polynom resExpected = new Polynom(2, 4, 3);
+
+            Polynom result = p1 + p2;
+        }
+
+        [Test]
+        public void SumOfFirstAndSecondOrderPolynoms1Test()
+        {
+            Polynom resExpected = new Polynom(2.4, 4.8, 3.7);
+
+            Polynom p1 = new Polynom(1.1, 2.3);
+            Polynom p2 = new Polynom(1.3, 2.5, 3.7);
+            Polynom result = p1 + p2;
+            bool b = result == resExpected;
+            Assert.True((p1 + p2) == resExpected, "add result is wrong");
+        }
+
+        ////TODO: test == operator, for equal, for not equal
+        ////TODO: test != operator, for equal, for not equal
+    }
+}
