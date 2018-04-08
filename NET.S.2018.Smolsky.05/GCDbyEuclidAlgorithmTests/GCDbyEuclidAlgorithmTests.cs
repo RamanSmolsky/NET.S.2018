d4f068e35436ef9330ab774d9268b1bd53e229c5
Raman.Smolsky <Raman.Smolsky@gmail.com>
2018-04-08 21:16:21 +0300

    Day 5 - added initial version of the GCD Euclid algorithm

---
+++ NET.S.2018.Smolsky.05/GCDbyEuclidAlgorithmTests/GCDbyEuclidAlgorithmTests.cs
@@ -0,0 +1,27 @@
+﻿using GCDbyEuclidAlgorithm;
+using NUnit.Framework;
+
+namespace GCDbyDifferentAlgorithmsTests
+{
+    [TestFixture]
+    public class GCDbyEuclidAlgorithmTests
+    {
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(-1, ExpectedResult = 1)]
+        [TestCase(1, 1, ExpectedResult = 1)]
+        [TestCase(1, 2, ExpectedResult = 1)]
+        [TestCase(2, 2, ExpectedResult = 2)]
+        [TestCase(-2, 2, ExpectedResult = 2)]
+        [TestCase(-2, -2, ExpectedResult = 2)]
+        [TestCase(-2, -3, ExpectedResult = 1)]
+        [TestCase(0, 5, ExpectedResult = 5)]
+        [TestCase(15, 0, ExpectedResult = 15)]
+        [TestCase(-5, 10, ExpectedResult = 5)]
+        [TestCase(5, 10, 15, ExpectedResult = 5)]
+        [TestCase(11, 121, 1331, 14641, ExpectedResult = 11)]
+        public int GcdOneAndOneTest(params int[] elements)
+        {
+            return GCD.CalculateGCD(elements);
+        }
+    }
+}
---
+++ NET.S.2018.Smolsky.05/NET.S.2018.Smolsky.05/GCD.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace GCDbyEuclidAlgorithm
+{
+    public static class GCD
+    {
+        /// <summary>
+        /// Calculates greatest common divisor for an array of integers provided, using Euclid algorithm
+        /// </summary>
+        /// <param name="elements">list of elements</param>
+        /// <returns><see cref="int"/></returns>
+        public static int CalculateGCD(params int[] elements)
+        {
+            if (elements == null)
+            {
+                throw new ArgumentNullException($"{nameof(elements)} should not be null");
+            }
+
+            if (elements.Length == 1)
+            {
+                return Math.Abs(elements[0]);
+            }
+
+            if (elements.Length == 2)
+            {
+                return CalculateGCD(elements[0], elements[1]);
+            }
+
+            var next = Array.CreateInstance(typeof(int), elements.Length - 1);
+
+            Array.Copy(elements, 1, next, 0, elements.Length - 1);
+
+            return CalculateGCD(elements[0], CalculateGCD((int[])next));
+        }
+
+        /// <summary>
+        /// Calculates greatest common divisor for two integers provided, using Euclid algorithm
+        /// </summary>
+        /// <param name="a">first integer</param>
+        /// <param name="b">second integer</param>
+        /// <returns><see cref="int"/></returns>
+        private static int CalculateGCD(int a, int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            if (a == 0 & b != 0)
+            {
+                return b;
+            }
+            else if (b == 0 & a != 0)
+            {
+                return a;
+            }
+
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+
+            return a;
+        }
+    }
+}
