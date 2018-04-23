a11d8b8acf5e8e186744795110784be44526f359
Raman.Smolsky <Raman.Smolsky@gmail.com>
2018-04-23 09:43:49 +0300

    VerifyPassword solution updated

---
+++ NET.W.2018.RamanSmolsky.Test/Task1.Solution.Tests/VerifyPasswordTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static Task1.Solution.PasswordCheckerService;
+
+namespace Task1.Solution.Tests
+{
+    [TestFixture]
+    class VerifyPasswordTests
+    {
+
+        [Test]
+        public void VerifyPassword_ContainsCatDog_True_Test()
+        {
+            PasswordValidator validators = new PasswordValidator(PasswordContainsCat);
+            validators += PasswordContainsDog;
+
+            IRepository rep = new SqlRepository();
+
+            PasswordCheckerService service = new PasswordCheckerService(rep);
+
+            Assert.True(service.VerifyPassword("catdog", validators).Item1);
+        }
+
+        [Test]
+        public void VerifyPassword_ContainsCatDog_False_Test()
+        {
+            PasswordValidator validators = new PasswordValidator(PasswordContainsCat);
+            validators += PasswordContainsDog;
+
+            IRepository rep = new SqlRepository();
+
+            PasswordCheckerService service = new PasswordCheckerService(rep);
+
+            Assert.False(service.VerifyPassword("catdg", validators).Item1);
+        }
+
+        private bool PasswordContainsCat(string password)
+        {
+            return password.ToUpper().Contains("CAT");
+        }
+
+        private bool PasswordContainsDog(string password)
+        {
+            return password.ToUpper().Contains("DOG");
+        }
+    }
+}
---
+++ NET.W.2018.RamanSmolsky.Test/Task1.Solution/PasswordCheckerService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+namespace Task1.Solution
+{
+    public class PasswordCheckerService
+    {
+        public delegate bool PasswordValidator(string password);
+
+        private IRepository _repository;
+
+        public Tuple<bool, string> VerifyPassword(string password)
+        {
+            if (password == null)
+                throw new ArgumentException($"{password} is null arg");
+
+            if (password == string.Empty)
+                return Tuple.Create(false, $"{password} is empty ");
+
+            // check if length more than 7 chars 
+            if (password.Length <= 7)
+                return Tuple.Create(false, $"{password} length too short");
+
+            // check if length more than 10 chars for admins
+            if (password.Length >= 15)
+                return Tuple.Create(false, $"{password} length too long");
+
+            // check if password conatins at least one alphabetical character 
+            if (!password.Any(char.IsLetter))
+                return Tuple.Create(false, $"{password} hasn't alphanumerical chars");
+
+            // check if password conatins at least one digit character 
+            if (!password.Any(char.IsNumber))
+                return Tuple.Create(false, $"{password} hasn't digits");
+
+            _repository.Create(password);
+
+            return Tuple.Create(true, "Password is Ok. User was created");
+        }
+
+        public Tuple<bool, string> VerifyPassword(string password, PasswordValidator validator)
+        {
+            if (password == null)
+                throw new ArgumentException($"{password} is null arg");
+
+            if (password == string.Empty)
+                return Tuple.Create(false, $"{password} is empty ");
+
+            if (validator == null)
+            {
+                return VerifyPassword(password);
+            }
+
+            bool result = true;
+
+            foreach (PasswordValidator rule in validator.GetInvocationList())
+            {
+                result &= rule(password);                
+            }
+
+            if (!result)
+                return Tuple.Create(false, $"{password} is not valid");
+
+            _repository.Create(password);
+
+            return Tuple.Create(true, "Password is Ok. User was created");
+        }
+
+        public PasswordCheckerService(IRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
